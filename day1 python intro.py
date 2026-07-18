f69b36dcfec504a49d94798e7ed54a6fc3f362d5
SaitrishankAUCSE <113962653+SaitrishankAUCSE@users.noreply.github.com>
2026-07-18 10:45:59 +0530

    Add files via upload

---
+++ day1 python intro.py
@@ -0,0 +1,68 @@
+'''
+python
+
+
+	- in the year 1991 by russon
+
+
+
+
+what is python
+		- high level language
+		- oops
+		- interpreted language 
+					:- the code will be executed line by line that's the reason python is a interpreted language
+				        example :- num=9
+						   print(num)
+						   sum=90
+						   print(num)
+		- dynamically typed language
+					:- no need to mention the type of data pass to the variable
+					example :-   num = [5,6]
+    					   	     print(type(num))
+
+
+why python
+		- easy to learn
+		- less syntax
+		- easy to understand
+		- cross platform
+		- open-source
+		-large number of libraries
+
+
+comments
+		- single-line (#) :- this is used to explain line in the code
+		- multi-line :- used to comment multiple line using (''' ''',""" """)
+
+
+variables
+		- this is used to store the datatypes 
+		- we can start with :- "_", "small letters", "capital letters"
+		- we cannot start with :- "special  characters", "number at first position", "space"
+
+
+Boolean types
+		- example :- num = 9
+			     num_2 = 89
+			     print(num != num_2)
+
+
+multiple variables with single value
+						example :- num = num_2 = num_3 = 78
+							   print(num_2)
+
+multiple variables with multiple value
+						example :- num,num2 = 9,90 
+							   print(num)
+							   print(num_2)
+
+swapping variable without using third variable
+						example :- num = 7
+   							   num_2 = 70
+							   num,num_2 = num_2,num
+                                                           #7,70 = 70,7
+							   print(num)
+
+
+'''
---
+++ day2 operators.py
@@ -0,0 +1,63 @@
+'''''
+                  Tokens
+                 --------
+-> Tokens are the smallest individual parts or units in the code or in the program
+---------------------------------------------------------------------------------------
+
+1. Keywords or reserved words
+ -> eg: if, for, else, return, while, and, in
+ --------------------------------------------
+2. identifiers
+ -> identifiers or variable, function name, class name
+ ------------------------------------------------------
+3. operators
+ -> eg: +, -, =
+ --------------------------------------------------
+4. literal or data types
+ -> eg: int, str, tuple, list
+
+
+                  Operators
+                ------------
+
+1. Arithmetic operator:
+ -> -, +, /, %, *, **
+ 
+2. Assignment operator:
+ -> =, +=, -=, *=, /=, %=
+ 
+3. Comparision operator:
+ -> ==, !=, <=, >=, >, <
+ 
+4. Logical operator:
+ -> and, or, not
+ eg : num = 8
+      num2 = 7
+      print(num > num2 and num2 < 100)
+      print(num > num2 or num2 < 100)
+      print(num > num2 and num2 < 100)
+ 
+5. Identity operator:
+ -> is
+    is not
+    eg: so = [1,2]
+        how = [1,2]
+        print(id(so))
+        print(id(how))
+        print(so is how)
+        print(so == how)
+     
+6. Membership operator:
+ -> in
+    not in
+7. Bitwise operator:
+&, |, ^, <<, >>
+
+
+
+
+'''
+
+
+
+
